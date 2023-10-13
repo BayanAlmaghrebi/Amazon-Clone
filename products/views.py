0357212e6ed4e2771d1cfa374655da7760223282
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-10-13 12:49:25 +0200

    add Brand Detail

--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from .models import Product , Brand , Review
+
+
+def brand_list(request):
+    data = Brand.objects.all()  #query --> method--> change data main query
+    return render(request,'html',{'brands':data}) #{'brands':data} --> context --> method(extra data) , html = template
 
 
 class ProductList(generic.ListView):
@@ -13,3 +20,21 @@ class ProductDetail(generic.DetailView):
 
 class BrandList(generic.ListView):
     model = Brand
+
+
+class BrandDetail(generic.ListView):
+    model = Product
+    template_name = 'products/brand_detail.html'
+
+    #override main query to gett product for brand coming from url
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        queryset = super().get_queryset().filter(brand=brand)
+        return queryset
+    
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["brand"] = Brand.objects.get(slug=self.kwargs['slug'])
+        return context
+    
