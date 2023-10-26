9119c01a22a8d540f0ac726bf7be36021d1b1f82
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-10-26 23:59:26 +0200

    show Foreignkey as object

--- products/serializers.py
+++ products/serializers.py
@@ -4,8 +4,15 @@ from rest_framework import serializers
 from .models import Product , Brand , Review , ProductImages
 
 
+
+class BrandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Brand
+        fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
-    brand = serializers.StringRelatedField()
+    brand = BrandSerializer()
     class Meta:
         model = Product
         fields = '__all__'
@@ -14,7 +21,3 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 
-class BrandSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Brand
-        fields = '__all__'
