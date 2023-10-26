2ebc2137fa5cf16a9489502421689d57ca9efcd8
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-10-26 23:51:56 +0200

    add custom filter

--- products/api.py
+++ products/api.py
@@ -5,15 +5,17 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import ProductSerializer,BrandSerializer
 from .models import Product , Brand
 from .mypagination import MyPagination
+from .myfilter import ProductFilter
 
 
 class ProductListAPI(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
-    filterset_fields = ['flag', 'brand']
+    filterset_fields = ['flag', 'brand', 'price']
     search_fields = ['name', 'subtitle', 'description']
     ordering_fields = ['price', 'quantity']
+    filterset_class = ProductFilter
 
 
 class ProductDetailAPI(generics.RetrieveAPIView):
