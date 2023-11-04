f5e1c30b2787c15a3c9f7619f1ae18d994090529
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-11-04 15:30:19 +0100

    add home data

--- settings/views.py
+++ settings/views.py
@@ -5,14 +5,14 @@ from products.models import Brand , Product , Review
 def home(request):
     brands = Brand.objects.all()[:10]     # [:10] يعني احضار اول 10 براندات
     sale_products = Product.objects.filter(flag='Sale')[:10]
-    feature_products = Product.objects.filter(flag='Feature')[:6]
+    feature_products = Product.objects.filter(flag='Feature')[:5]
     new_products = Product.objects.filter(flag='New')[:10]
-    Review= Review.objects.all()[:6]
+    reviews= Review.objects.all()[:6]
 
     return render(request,'settings/home.html',{
         'brands': brands,
         'sale_products': sale_products,
         'feature_products': feature_products,
         'new_products': new_products, 
-        'Review': Review
+        'reviews': reviews 
     })
