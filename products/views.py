a1244e232eb7e5a54b01cad49c2523d7f9322dcc
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-11-09 23:38:26 +0100

    add django queryset api

--- products/views.py
+++ products/views.py
@@ -3,6 +3,8 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import generic
 from .models import Product , Brand , Review
+from django.db.models import Q , Value , F
+from django.db.models.aggregates import Count,Min,Max,Avg,Sum
 
 
 def brand_list(request):
@@ -11,7 +13,53 @@ def brand_list(request):
 
 
 def mydebug(request):
-    data = Product.objects.select_related('brand').all()
+    # data = Product.objects.select_related('brand').all()
+    # data = Product.objects.filter(price__gte=100)
+    # data = Product.objects.filter(price__lte=100)
+    # data = Product.objects.filter(price__range=(20,22))
+
+    # data = Product.objects.filter(brand__id__gt = 50)
+
+    # data = Product.objects.filter(name__contains = 'Hill')
+    # data = Product.objects.filter(name__startswith = 'Denise')
+    # data = Product.objects.filter(name__endswith = 'Hill')
+    # data = Product.objects.filter(name__isnull = True)
+
+    # data = Product.objects.filter(price__gte =80 , name__endswith = 'Hill')
+
+    # data = Product.objects.filter(
+    #     Q(price__gte = 99) |
+    #     Q(name__endswith = 'Hill')
+    # )
+
+    # data = Product.objects.order_by('price')
+    data = Product.objects.order_by('-price')
+    # data = Product.objects.order_by('name','-price')
+    # data = Product.objects.filter(flag='New').order_by('-price')
+
+    # data = Product.objects.order_by('price')[0]  [-1]
+    # data = Product.objects.earliest('price')
+    # data = Product.objects.latest('price')
+    # data = Product.objects.all()[:10]
+
+    # data = Product.objects.values('name','flag')
+    # data = Product.objects.only('name','flag')
+    # data = Product.objects.defer('quantity')
+
+    # data = Product.objects.select_related('brand').all()   # foreignkey , one-to-one
+    # data = Product.objects.prefetch_related('brand').all()   # many to many
+
+    # aggregation : min max sum avg count
+    # data = Product.objects.aggregate(myavg = Avg('price'))
+    # data = Product.objects.aggregate(Count('quantity'))
+    # data = Product.objects.aggregate(Count('id'))
+
+    # data = Product.objects.aggregate(Sum('quantity'))
+
+    # data = Product.objects.aggregate(Min('price'))
+
+
+    # data = Product.objects.annotate(price_with_tax=F('price')*1.25)
     return render(request, 'products/debug.html',{'data':data})
 
 
