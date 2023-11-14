014d08fde1ba4792b294ba0c731b3533e747bc65
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-11-14 09:56:57 +0100

    design order_detail part 1

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 import datetime
-
+from products.models import Product
 
 ORDER_STATUS = (
     ('Recieved','Recieved'),
@@ -21,11 +21,24 @@ class Order(models.Model):
     coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL, null=True,blank=True)
     order_total_discount = models.FloatField(null=True,blank=True)
 
+
+
+
     def __str__(self):
         return str(self.user)
 
+
+
+
+
+
 class OrderDetail(models.Model):
-    pass
+    order = models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name='orderdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = ''
+    brand = ''
+    price = ''
+    total = ''
 
 
 
