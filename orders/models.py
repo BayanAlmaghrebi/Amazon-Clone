e38a812ff7d286e095c01bb9f137a7dac4b9d04e
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-11-20 13:13:27 +0100

    add cart & cart detail models

--- orders/models.py
+++ orders/models.py
@@ -29,10 +29,6 @@ class Order(models.Model):
         return str(self.user)
 
 
-
-
-
-
 class OrderDetail(models.Model):
     order = models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
     product = models.ForeignKey(Product,related_name='orderdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
@@ -41,6 +37,30 @@ class OrderDetail(models.Model):
     total = models.FloatField()
 
 
+
+Cart_STATUS = (
+    ('Inprogress','Inprogress'),
+    ('Completed','Completed'),
+)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,related_name='Cart_user',on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=15 , choices=Cart_STATUS,default='Recieved')
+    coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL, null=True,blank=True)
+    Cart_total_discount = models.FloatField(null=True,blank=True)
+
+
+
+
+    def __str__(self):
+        return str(self.user)
+
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart,related_name='cart_detail',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,related_name='cartdetail_product',on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = models.IntegerField()
+    total = models.FloatField()
 
 class Coupon(models.Model):
     code = models.CharField(max_length=15)
