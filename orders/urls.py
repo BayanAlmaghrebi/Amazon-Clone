a3749670341ebd797691f68dbb23d2818c94b88b
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-12-01 18:45:07 +0100

    add order list api

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import add_to_cart ,order_list ,checkout ,process_payment, payment_success, payment_failed
 
+from .api import OrderListAPI
+
 app_name = 'orders'
 
 
@@ -10,5 +12,9 @@ urlpatterns = [
     path('checkout/payment' , process_payment),
     path('checkout/payment/success' , payment_success),
     path('checkout/payment/failed' , payment_failed),
-    path('add-to-cart' , add_to_cart , name='add-to-cart')
+    path('add-to-cart' , add_to_cart , name='add-to-cart'),
+
+
+       # api
+    path('api/<str:username>' , OrderListAPI.as_view()),
 ]
