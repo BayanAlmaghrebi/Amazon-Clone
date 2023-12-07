c2596b5e7299a2ea53baf49eb1ca91fa6e48e3bb
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-12-07 23:37:35 +0100

    add translation in template

--- project/urls.py
+++ project/urls.py
@@ -41,7 +41,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('',home),
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path('products/' , include('products.urls')),
     path('orders/' , include('orders.urls',namespace='orders')),
 
@@ -53,7 +53,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api-auth/', include('dj_rest_auth.urls')),
-    path('api-auth/signup/', include('dj_rest_auth.registration.urls')),
+    path('api-auth/signup/', include('dj_rest_auth.registration.urls'))
 
 ]
 
