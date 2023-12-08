eb4d5443d7c32fd9d4e16113ab8bf04ad034513a
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-12-08 17:51:30 +0100

    design user models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import DeliveryAddress
+from .models import DeliveryAddress , Profile , Phones
 
 
+
+
+admin.site.register(Profile)
+admin.site.register(Phones)
 admin.site.register(DeliveryAddress)
