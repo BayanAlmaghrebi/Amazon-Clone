ea289e4173d847299ddcb206c4b8adf9fb555e89
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-12-20 22:14:48 +0100

    signup part2  with activate and send email

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render , redirect
 from .models import Profile
-from .forms import SignupForm
+from .forms import SignupForm , ActivateForm
 from django.core.mail import send_mail
+from django.contrib.auth.models import User 
 
 def signup(request):
     
@@ -32,8 +33,27 @@ def signup(request):
     else:
         form = SignupForm()
 
-    return render(request,'signup.html',{'form':form})
+    return render(request,'accounts/signup.html',{'form':form})
     
 
-def activated(request,username):
-    pass 
+def activate(request,username):
+    profile = Profile.objects.get(user__username=username) 
+
+    if request.method == 'POST':
+        form = ActivateForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            if code == profile.code :
+                profile.code = ''
+                profile.save()
+
+                user = User.objects.get(username=username)
+                user.is_active = True
+                user.save()
+                return redirect('/accounts/login')
+    
+    else:
+        form = ActivateForm()
+
+    return render(request,'accounts/activate.html',{'form':form})
+     
