94b8a0e89c4c8a9c770cb162621d7a174e2b77d3
Bayan Almaghrebi <bayanalmaghrebi@gmail.com>
2023-12-22 00:09:12 +0100

    add gmail settings to send email

--- project/settings.py
+++ project/settings.py
@@ -206,4 +206,11 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com' #service
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "bayanalmaghrebi@gmail.com"
+EMAIL_HOST_PASSWORD = "cjajwyezvvhgnayj"
